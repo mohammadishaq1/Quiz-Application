7e0d42030e28ad76b7adff4072e5d1a9a84eea03
Mohammad Ishaq <info@ishaq.io>
2020-02-25 15:35:10 -0600

    added to database

--- Quiz Application/Controllers/AdminController.cs
+++ Quiz Application/Controllers/AdminController.cs
@@ -29,5 +29,37 @@ namespace Quiz_Application.Controllers
                                                    }).ToList();
             return View(objCategoryViewModel);
         }
+        [HttpPost]
+        public JsonResult Index(QuestionOptionViewModel QuestionOption)
+        {
+            Question objQuestion = new Question();
+
+            objQuestion.QuestionName = QuestionOption.QuestionName;
+            objQuestion.CategoryId = QuestionOption.CategoryId;
+            objQuestion.isActive = true;
+            objQuestion.isMultiple = false;
+            ObjQuizDBEntities.Questions.Add(objQuestion);
+            ObjQuizDBEntities.SaveChanges();
+
+            int questionId = objQuestion.QuestionId;
+
+            foreach(var item in QuestionOption.ListOfOptions)
+            {
+                Option objOption = new Option();
+                objOption.OptionName = item;
+                objOption.QuestionId = questionId;
+                ObjQuizDBEntities.Options.Add(objOption);
+                ObjQuizDBEntities.SaveChanges();
+
+            }
+
+            Answer objAnswer = new Answer();
+            objAnswer.AnswerText = QuestionOption.AnswerText;
+            objAnswer.QuestionId = questionId;
+            ObjQuizDBEntities.Answers.Add(objAnswer);
+            ObjQuizDBEntities.SaveChanges();
+
+            return Json(new { message = "Data successfully added", success = true }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ Quiz Application/ViewModel/QuestionOptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiz_Application.ViewModel
+{
+    public class QuestionOptionViewModel
+    {
+        public int CategoryId { get; set; }
+        public string QuestionName { get; set; }
+        public List<string> ListOfOptions { get; set; }
+        public string AnswerText { get; set; }
+    }
+
+   
+}
