52c070f4977c75cc04cc8b4f12b2d92455d79299
Mohammad Ishaq <info@ishaq.io>
2020-02-25 11:20:17 -0600

    Admin view

--- Quiz Application/Controllers/AdminController.cs
+++ Quiz Application/Controllers/AdminController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Quiz_Application.Models;
+using Quiz_Application.ViewModel;
 
 namespace Quiz_Application.Controllers
 {
     [Authorize]
     public class AdminController : Controller
     {
+        private QuizDBEntities ObjQuizDBEntities;
+
+        public AdminController()
+        {
+            ObjQuizDBEntities = new QuizDBEntities();
+        }
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            CategoryViewModel objCategoryViewModel = new CategoryViewModel();
+            objCategoryViewModel.ListofCategory = (from objCat in ObjQuizDBEntities.Categories
+                                                   select new SelectListItem()
+                                                   {
+                                                       Value = objCat.CategoryId.ToString(),
+                                                       Text = objCat.CategoryName
+                                                   }).ToList();
+            return View(objCategoryViewModel);
         }
     }
 }
---
+++ Quiz Application/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiz_Application.ViewModel
+{
+    public class CategoryViewModel
+    {
+        [Display(Name ="Category")]
+        [Required(ErrorMessage ="Required")]
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Question")]
+        public string QuestionName { get; set; }
+        public string OptionName { get; set; }
+        public string CategoryName { get; set; }
+        public IEnumerable<SelectListItem> ListofCategory { get; set; }
+    }
+}
